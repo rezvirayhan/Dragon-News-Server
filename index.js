af8132a2d703bc9ebd50be74eac044e42e7f78ad
rezvirayhan <devrezvi.rayhan@gmail.com>
2023-09-22 13:33:02 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const categories = require('./data/categories.json');
+const news = require('./data/news.json')
+
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Dragon Is Running ON SERVER')
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if (id === 0) {
+        res.send(news)
+    }
+    else {
+        const categoryNews = news.filter(n => parseInt(n.category_id) === id)
+        res.send(categoryNews)
+    }
+})
+app.listen(port, () => {
+    console.log(`Dragon Api Is Running On Port: ${port}`);
+
+})
